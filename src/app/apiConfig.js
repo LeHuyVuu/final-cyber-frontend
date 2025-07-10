d7bf471863637a6507ecd45fec571c12b63d7a7b
LeHuyVuu <lehuyvuok@gmail.com>
2025-07-10 11:49:06 +0700

    fix

--- src/app/apiConfig.js
+++ src/app/apiConfig.js
@@ -1,5 +1,5 @@
-const API_BASE_URL = "http://103.112.211.196:2609";
-// const API_BASE_URL = "https://localhost:44376";
+// const API_BASE_URL = "http://103.112.211.196:2609";
+const API_BASE_URL = "https://localhost:44376";
 
 
 export default API_BASE_URL;
